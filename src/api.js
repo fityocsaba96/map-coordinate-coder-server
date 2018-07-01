1d42d1b30e72def3e28a625d2256a2b2475e316c
Fityó Csaba <fityocsaba96@gmail.com>
2018-07-01 02:14:54 +0200

    add route prefix

--- src/api.js
+++ src/api.js
@@ -6,20 +6,21 @@ const api = express();
 api.use(express.json());
 const historyDb = new HistoryDb();
 const coder = new Coder();
+const router = express.Router();
 
-api.post('/encode', (request, response) => {
+router.post('/encode', (request, response) => {
     const encoded = coder.encode(request.body.coordinate, request.body.accuracy);
     historyDb.createEncoding(request.body.coordinate, encoded);
     response.send({ code: encoded }).end();
 });
 
-api.post('/decode', (request, response) => {
+router.post('/decode', (request, response) => {
     const decoded = coder.decode(request.body.code);
     historyDb.createDecoding(request.body.code, decoded);
     response.send(decoded).end();
 });
 
-api.get('/history', (request, response) => {
+router.get('/history', (request, response) => {
     const allHistory = historyDb.readAll();
     const allHistoryGrouped = { encode: [], decode: [] };
     allHistory.forEach(history => {
@@ -27,5 +28,7 @@ api.get('/history', (request, response) => {
     });
     response.send(allHistoryGrouped).end();
 });
+
+api.use('/api', router);
 
 module.exports = api;
